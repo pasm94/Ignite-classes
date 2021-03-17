25d10f42c83ac0f9041c6244edaef8515b6e911c
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-03-16 23:11:04 -0300

    fontes do google fonts

--- reactjs/dtmoney/src/styles/global.ts
+++ reactjs/dtmoney/src/styles/global.ts
@@ -12,6 +12,7 @@ export const GlobalStyle = createGlobalStyle`
     --background: #f0f2f5;
     --shape: #ffffff;
   }
+
   * {
     margin: 0;
     padding: 0;
@@ -36,6 +37,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: var(---background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
