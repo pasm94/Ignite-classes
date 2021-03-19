9aba6cde8b30a36b80e8e3a28369771a19b8ed29
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-03-19 07:42:08 -0300

    estrutura do formulario

--- reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 Modal.setAppElement('#root'); // a lib pede por questao de acessibilidade
 
@@ -11,8 +12,20 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName='react-modal-overlay'
+      className='react-modal-content'
+    >
+      <Container>
+        <h2>Cadastrar transação</h2>
+        <input placeholder='Título' />
+        <input type='number' placeholder='Valor' />
+        <input placeholder='Categoria' />
+
+        <button type='submit'>Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
