26ab853dbef81f7bcc6db7e22baab1373c11d420
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-03-21 04:15:37 -0300

    carregando transacoes

---
+++ reactjs/dtmoney/src/TransactionsContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from './services/api';
+
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  category: string;
+  type: 'deposit' | 'withdraw';
+  createdAt: string;
+}
+
+interface TransactionsProviderProps {
+  children: ReactNode /* ReactNode significa que aceita qlqr content valido pra react */;
+}
+
+export const TransactionsContext = createContext<Transaction[]>([]);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api
+      .get('/transactions')
+      .then(response => setTransactions(response.data.transactions));
+  }, []);
+
+  return (
+    <TransactionsContext.Provider value={transactions}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
