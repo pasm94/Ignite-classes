4e48224333a41c21b8e313d3ab126a5fe0c655ce
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-03-28 13:59:45 -0300

    estilos globais do app

--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -1,4 +1,3 @@
-import styles from '../styles/home.module.scss';
 import Head from 'next/head';
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
         <title>Início | ig.news</title>
       </Head>
 
-      <h1 className={styles.title}>Hello Word</h1>
+      <h1>Hello Word</h1>
     </>
   );
 }
