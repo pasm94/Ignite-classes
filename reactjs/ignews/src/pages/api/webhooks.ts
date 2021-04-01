c3389ce00cfdbeac80de19e3ce60d01de054e536
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-03-31 22:34:07 -0300

    salvando dados do evento

--- reactjs/ignews/src/pages/api/webhooks.ts
+++ reactjs/ignews/src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Readable } from 'stream';
 import Stripe from 'stripe';
 import { stripe } from '../../services/stripe';
+import { saveSubscription } from './_lib/manageSubscription';
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -42,7 +43,26 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log('Evento recebido', event);
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            );
+
+            break;
+          default:
+            throw new Error('Unhandled evet.');
+        }
+      } catch (err) {
+        // erro pq eh um tipo de evento que nao esta sendo tratado
+        // nao pode ser status de erro pq se n o stripe vai tentar executar o pagamento de novo
+        return res.json({ error: 'Webhook handler failed' });
+      }
     }
 
     res.json({ received: true });
