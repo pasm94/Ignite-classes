de827ae4434e269792abbcfb381b19462f37fd6e
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-04-03 00:47:20 -0300

    navegacao no menu

--- reactjs/ignews/src/components/Header/index.tsx
+++ reactjs/ignews/src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import Link from 'next/link';
 
 export function Header() {
   return (
@@ -7,10 +8,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src='/images/logo.svg' alt='ig.news' />
         <nav>
-          <a href='#' className={styles.active}>
-            Home
-          </a>
-          <a href='#'>Posts</a>
+          <Link href='/'>
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href='/posts'>
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
