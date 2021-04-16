04c209a6930f710a5ffbe7bb55491e93aa1d2ebf
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-04-16 01:12:44 -0300

    header responsivo

--- reactjs/dashgo/src/components/Header/Profile.tsx
+++ reactjs/dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
-        <Text>Paulo A. S. Müller</Text>
-        <Text color='gray.300' fontSize='small'>
-          paulo@example.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr='4' textAlign='right'>
+          <Text>Paulo A. S. Müller</Text>
+          <Text color='gray.300' fontSize='small'>
+            paulo@example.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size='md'
