186c0bd28a1e0fc95e295be0bcb0c1c20153c313
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-04-17 01:04:41 -0300

    fluxo de navegacao

--- reactjs/dashgo/src/components/Sidebar/NavLink.tsx
+++ reactjs/dashgo/src/components/Sidebar/NavLink.tsx
@@ -1,25 +1,30 @@
 import {
   Icon,
-  Link,
+  Link as ChakraLink,
   LinkProps as ChakraLinkProps,
   Text,
 } from '@chakra-ui/react';
 import { ElementType } from 'react';
+import Link from 'next/link';
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
   // ElementType -> quando eu passo o nome/referencia do componente (RiDashboardLine),
   // e nao a declaracao <RiDashboardLine/>
   children: string;
+  href: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display='flex' align='center' {...rest}>
-      <Icon as={icon} fontSize='20' />
-      <Text ml='4' fontWeight='medium'>
-        {children}
-      </Text>
+    <Link href={href} passHref>
+      {/* passHref mostra o link no hover, eh como colocar um <a> em volta de tudo */}
+      <ChakraLink display='flex' align='center' {...rest}>
+        <Icon as={icon} fontSize='20' />
+        <Text ml='4' fontWeight='medium'>
+          {children}
+        </Text>
+      </ChakraLink>
     </Link>
   );
 }
