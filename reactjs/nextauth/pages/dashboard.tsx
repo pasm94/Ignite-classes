8c5bf8321ffcdf48b1957d8ce40872d134f708cb
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-05-09 02:56:46 -0300

    criando componente de permissao

--- reactjs/nextauth/pages/dashboard.tsx
+++ reactjs/nextauth/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from 'react';
+import { Can } from '../components/Can';
 import { AuthContext } from '../contexts/AuthContext';
 import { useCan } from '../hooks/useCan';
 import { setupApiClient } from '../services/api';
@@ -7,10 +8,6 @@ import { withSSRAuth } from '../utils/withSSRAuth';
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
-
-  const userCanSeeMetrics = useCan({
-    roles: ['administrator', 'editor'],
-  });
 
   useEffect(() => {
     api
@@ -23,7 +20,9 @@ export default function Dashboard() {
     <>
       <h1>Dashboard {user?.email}</h1>
 
-      {userCanSeeMetrics && <div>Métricas</div>}
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
     </>
   );
 }
