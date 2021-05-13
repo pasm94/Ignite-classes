c947870a386518fa23d0a7beeeaa1be7b70b675f
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-05-12 23:50:50 -0300

    formatacao de dados

--- reactjs/perfreact/components/SearchResults.tsx
+++ reactjs/perfreact/components/SearchResults.tsx
@@ -1,25 +1,21 @@
-import { useMemo } from 'react';
 import { ProductItem } from './ProductItem';
 
 interface SearchResultsProps {
   results: Array<{
     id: number;
     price: number;
+    priceFormatted: string;
     title: string;
   }>;
   onAddToWishList: (id: number) => void;
+  totalPrice: number;
 }
 
 export function SearchResults({
   results,
   onAddToWishList,
+  totalPrice,
 }: SearchResultsProps) {
-  const totalPrice = useMemo(() => {
-    return results.reduce((total, product) => {
-      return total + product.price;
-    }, 0);
-  }, [results]);
-
   return (
     <div>
       <h2>{totalPrice}</h2>
