0576741bf20044c5a151e0b3b935c324bb4da8e4
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-05-13 23:36:25 -0300

    virtualizacao

--- reactjs/perfreact/classes-list.md
+++ reactjs/perfreact/classes-list.md
@@ -7,3 +7,8 @@
   - yarn create next-app nextauth
   - yarn add typescript @types/react @types/node -D
   - yarn add json-server -D
+
+- Virtualização
+
+  - yarn add react-virtualized
+  - yarn add @types/react-virtualized
--- reactjs/perfreact/components/SearchResults.tsx
+++ reactjs/perfreact/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { List, ListRowRenderer } from 'react-virtualized';
 import { ProductItem } from './ProductItem';
 
 interface SearchResultsProps {
@@ -16,10 +17,30 @@ export function SearchResults({
   onAddToWishList,
   totalPrice,
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map(product => {
+
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+      {/* {results.map(product => {
         return (
           <ProductItem
             key={product.id}
@@ -27,7 +48,7 @@ export function SearchResults({
             onAddToWishList={onAddToWishList}
           />
         );
-      })}
+      })} */}
     </div>
   );
 }
