aa63fc14f2045ef107654f810eec58514e5ddac0
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-05-29 22:27:11 -0300

    Testando Página Home

--- reactjs/ignews/src/components/SubscribeButton/index.tsx
+++ reactjs/ignews/src/components/SubscribeButton/index.tsx
@@ -1,5 +1,5 @@
 import { signIn, useSession } from 'next-auth/client';
-import { useRouter } from 'next/dist/client/router';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
