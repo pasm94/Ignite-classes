04571a8592721b631698a9cb368f91a8672d05f8
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-06-03 03:34:25 -0300

    Criando a estrutura do projeto

---
+++ nodejs/fundamentos-nodejs/classes-list.md
@@ -0,0 +1,10 @@
+## CHAPTER I
+
+#### Fundamentos do NodeJS
+
+- Criando a estrutura do projeto
+
+  - mkdir fundamentos-nodejs
+  - yarn init -y
+  - yarn add express
+  - node src/index.js (rodar o node)
---
+++ nodejs/fundamentos-nodejs/src/index.js
@@ -0,0 +1,10 @@
+const { request } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello world!' });
+});
+
+app.listen(3333);
