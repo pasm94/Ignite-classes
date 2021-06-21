ec719ba4634e04008627b1401d126be77cd3e0f4
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-06-21 03:08:47 -0300

    Criando migration de especificação

--- nodejs/rentx/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ nodejs/rentx/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -15,15 +15,15 @@ class CreateSpecificationUseCase {
   ) {}
   // o private nos permite ter acesso ao this. em toda class
 
-  execute({ name, description }: IRequest): void {
+  async execute({ name, description }: IRequest): Promise<void> {
     const specificationAlreadyExists =
-      this.specificationsRepository.findByName(name);
+      await this.specificationsRepository.findByName(name);
 
     if (specificationAlreadyExists) {
       throw new Error("Specification already exists!");
     }
 
-    this.specificationsRepository.create({
+    await this.specificationsRepository.create({
       name,
       description,
     });
