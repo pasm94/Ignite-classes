07f101fbdf032f363b594c255aadce7f6b013b0c
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-08-31 17:16:10 -0300

    Criando o User test

---
+++ elixir/exlivery/test/user_test.exs
@@ -0,0 +1,45 @@
+defmodule Exlivery.Users.UserTest do
+  use ExUnit.Case
+
+  alias Exlivery.Users.User
+
+  describe "build/5" do
+    test "when all params are valid, returns the user" do
+      response =
+        User.build(
+          "Address test",
+          "Name test",
+          "email@test.com",
+          "123.456.789-00",
+          18
+        )
+
+      expected_response =
+        {:ok,
+         %User{
+           address: "Address test",
+           age: 18,
+           cpf: "123.456.789-00",
+           email: "email@test.com",
+           name: "Name test"
+         }}
+
+      assert response == expected_response
+    end
+
+    test "when there are invalid params, returns an error" do
+      response =
+        User.build(
+          "Address test",
+          "Name test invalid age",
+          "email@test.com",
+          "123.456.789-00",
+          15
+        )
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+  end
+end
