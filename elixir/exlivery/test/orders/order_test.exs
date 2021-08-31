90e7d774f85b591d5874c55cc8ad3b7916dcb694
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-08-31 18:11:25 -0300

    Testando a struct de Order

---
+++ elixir/exlivery/test/orders/order_test.exs
@@ -0,0 +1,36 @@
+defmodule Exlivery.Orders.OrderTest do
+  use ExUnit.Case
+
+  alias Exlivery.Orders.Order
+
+  import Exlivery.Factory
+
+  describe "build/2" do
+    test "when all params are valid, returns an order" do
+      user = build(:user)
+
+      items = [
+        build(:item),
+        build(:item, description: "Pizza test 2", quantity: 5)
+      ]
+
+      response = Order.build(user, items)
+
+      expected_response = {:ok, build(:order)}
+
+      assert response == expected_response
+    end
+
+    test "when there is no items in the order, returns an error" do
+      user = build(:user)
+
+      items = []
+
+      response = Order.build(user, items)
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+  end
+end
--- elixir/exlivery/test/support/factory.ex
+++ elixir/exlivery/test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
   alias Exlivery.Users.User
 
   def user_factory do
@@ -20,6 +20,18 @@ defmodule Exlivery.Factory do
       description: "Pizza test",
       quantity: 3,
       unity_price: Decimal.new("22.99")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Address test",
+      items: [
+        build(:item),
+        build(:item, description: "Pizza test 2", quantity: 5)
+      ],
+      total_price: Decimal.new("183.92"),
+      user_cpf: "123.456.789-00"
     }
   end
 end
