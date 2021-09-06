4b4a095eed3f06edc7767a97e9dadb55d42c7b4d
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-09-06 17:15:42 -0300

    Criando a rota de criação de usuários pt 2

--- elixir/rockelivery/lib/rockelivery_web/router.ex
+++ elixir/rockelivery/lib/rockelivery_web/router.ex
@@ -9,7 +9,7 @@ defmodule RockeliveryWeb.Router do
     pipe_through :api
 
     get "/", WelcomeController, :index
-    resources "/user", UsersController, except: [:new, :edit]
+    resources "/users", UsersController, except: [:new, :edit]
   end
 
   # Enables LiveDashboard only for development
