45f75bfab7d33baf153f44cd09c513c931682473
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-09-21 08:54:39 -0300

    Validando o CEP na criação do Usuário

--- elixir/rockelivery/lib/rockelivery/users/create.ex
+++ elixir/rockelivery/lib/rockelivery/users/create.ex
@@ -1,7 +1,14 @@
 defmodule Rockelivery.Users.Create do
-  alias Rockelivery.{Error, Repo, User}
+  alias Rockelivery.{Error, Repo, User, ViaCep.Client}
 
-  def call(params) do
+  def call(%{"cep" => cep} = params) do
+    case Client.get_cep_info(cep) do
+      {:ok, _cep_info} -> create_user(params)
+      {:error, _result} = error -> error
+    end
+  end
+
+  defp create_user(params) do
     params
     |> User.changeset()
     |> Repo.insert()
