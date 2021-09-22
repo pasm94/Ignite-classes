84216fb520aab64d498768caff93e4f070e9d3a1
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-09-22 09:07:18 -0300

    Criando o teste do nosso client pt 2

--- elixir/rockelivery/lib/via_cep/client.ex
+++ elixir/rockelivery/lib/via_cep/client.ex
@@ -22,7 +22,7 @@ defmodule Rockelivery.ViaCep.Client do
   end
 
   defp handle_get({:ok, %Env{status: 400, body: _body}}) do
-    {:error, Error.build(:not_found, "Invalid CEP!")}
+    {:error, Error.build(:bad_request, "Invalid CEP!")}
   end
 
   defp handle_get({:error, reason}) do
