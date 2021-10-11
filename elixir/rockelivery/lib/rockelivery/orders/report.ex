70298bfdcc84194bf0af965e894a3124f097c316
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-10-11 07:54:44 -0300

    Calculando o preço total e salvando o relatório

--- elixir/rockelivery/lib/rockelivery/orders/report.ex
+++ elixir/rockelivery/lib/rockelivery/orders/report.ex
@@ -1,25 +1,36 @@
 defmodule Rockelivery.Orders.Report do
   import Ecto.Query
 
-  alias Rockelivery.{Item, Order, Repo}
+  alias Rockelivery.{Item, Order, Orders.TotalPrice, Repo}
 
   @default_block_size 500
 
   def create(filename \\ "report.csv") do
     query = from order in Order, order_by: order.user_id
 
-    Repo.transaction(fn ->
-      query
-      |> Repo.stream(max_rows: @default_block_size)
-      |> Stream.chunk_every(@default_block_size)
-      |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
-      |> Enum.map(&parse_line/1)
-    end)
+    {:ok, order_list} =
+      Repo.transaction(
+        fn ->
+          query
+          |> Repo.stream(max_rows: @default_block_size)
+          |> Stream.chunk_every(@default_block_size)
+          |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
+          |> Enum.map(&parse_line/1)
+        end,
+        timeout: :infinity
+      )
+
+    # o exemplo infinity eh para n dar timeout caso a transaction dure mais que 15s (padrão)
+
+    File.write(filename, order_list)
   end
 
   defp parse_line(%Order{user_id: user_id, payment_method: payment_method, items: items}) do
+    total_price = TotalPrice.calculate(items)
+
     items_string = Enum.map(items, &item_string/1)
-    "#{user_id},#{payment_method},#{items_string}#{50.0}\n"
+
+    "#{user_id},#{payment_method},#{items_string}#{total_price}\n"
   end
 
   defp item_string(%Item{category: category, description: description, price: price}) do
